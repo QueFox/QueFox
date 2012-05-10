10dbdb19fecea0c012a36f871b897144d6f8e55d
Mike Synnott <mike@quotefish.com>
2012-05-10 12:31:16 +0100

    Moved Google Maps-related JS code out to its own JS file.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAqCKnvVvjuoqdJu9_BosVcUkvKAo0iSBs&libraries=places&sensor=true"></script>
 		<script src="js/dw_scroll_c.js" type="text/javascript"></script>
+		<script src="js/GoogleMaps.js" type="text/javascript"></script>
 		<!--<script type="text/javascript" src="js/jquery.watermark.js"></script>-->
 		<script>
 		
@@ -149,65 +150,6 @@
 			}
 		}
 		
-		</script>
-		<script>
-			function initialize() {
-				var mapOptions = {
-					center: new google.maps.LatLng(52.982427,-6.061436),
-					zoom: 15,
-					disableDefaultUI: true,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				};
-				var map = new google.maps.Map(document.getElementById('consumerLocationMap'), mapOptions);
-				
-				var enterLocation = document.getElementById('enterConsumerLocation');
-				//var options = {componentRestrictions: {country: 'ie'}};
-				var autocomplete = new google.maps.places.Autocomplete(enterLocation);
-				
-				autocomplete.bindTo('bounds', map);
-
-				//var infowindow = new google.maps.InfoWindow();
-				var marker = new google.maps.Marker({
-				  map: map
-				});
-
-				google.maps.event.addListener(autocomplete, 'place_changed', function() {
-				  //infowindow.close();
-				  var place = autocomplete.getPlace();
-				  if (place.geometry.viewport) {
-					map.fitBounds(place.geometry.viewport);
-				  } else {
-					map.setZoom(15);
-					map.setCenter(place.geometry.location);
-					
-				  }
-
-				  var image = new google.maps.MarkerImage(
-					  place.icon,
-					  new google.maps.Size(71, 71),
-					  new google.maps.Point(0, 0),
-					  new google.maps.Point(17, 34),
-					  new google.maps.Size(35, 35));
-				  marker.setIcon(image);
-				  marker.setPosition(place.geometry.location);
-
-				  var address = '';
-				  if (place.address_components) {
-					address = [(place.address_components[0] &&
-								place.address_components[0].short_name || ''),
-							   (place.address_components[1] &&
-								place.address_components[1].short_name || ''),
-							   (place.address_components[2] &&
-								place.address_components[2].short_name || '')
-							  ].join(' ');
-				  }
-
-				  $isConsumerLocationValid = true;
-				  //infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-				  //infowindow.open(map, marker);
-				});
-			}
-			google.maps.event.addDomListener(window, 'load', initialize);
 		</script>
 	</head>
 	<body>
