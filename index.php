8160c23032e53bfe3489e4061990ee018f5a14dd
Mike Synnott <mike@quotefish.com>
2012-05-11 10:51:26 +0100

    Local JQueryUI themes and further changes to BadCategory dialog

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<head>
 		<title>Quotefish&trade;</title>
 		<link rel="Stylesheet" href="css/quotefish.css" type="text/css" media="screen"/>
-		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/start/jquery-ui.css" rel="stylesheet" type="text/css"/>
+		<link rel="stylesheet" href="css/jquery-ui-themes-1.8.20/themes/trontastic/jquery-ui.css" type="text/css"/>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAqCKnvVvjuoqdJu9_BosVcUkvKAo0iSBs&libraries=places&sensor=true"></script>
@@ -181,8 +181,22 @@
 
 			function handleInvalidCategory(categoryText)
 			{
-					$( "#dialog:ui-dialog" ).dialog( "destroy" );
+				var narrative = 'We don\'t have the category \'' + categoryText +'\' yet.';
+				narrative += '<p/>Enter your email address below and click \'Notify Me\' if you would like us to send you an email when we have added this, or a similar category.';
+				narrative += '<p/>Otherwise click \'Cancel\' to enter a different category.'; 
+				
 					
+				var $dialog = 
+					$('<div id="badCategoryDialog"></div>')
+					.html('<div id="narrative">' + narrative + '</div>')
+					.dialog(
+							{
+								autoOpen: false,
+								title: 'Oops!',
+								modal: true
+							});
+					
+				/*
 					var name = $( "#name" ),
 						email = $( "#email" ),
 						password = $( "#password" ),
@@ -190,7 +204,7 @@
 						tips = $( ".validateTips" );
 
 					
-					$( "#dialog-form" ).dialog({
+					$( "#badCategorydialog" ).dialog({
 						autoOpen: false,
 						height: 300,
 						width: 350,
@@ -204,10 +218,7 @@
 								bValid = bValid && checkLength( email, "email", 6, 80 );
 								bValid = bValid && checkLength( password, "password", 5, 16 );
 
-								bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-								// From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-								bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-								bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+								//bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
 
 								if ( bValid ) {
 									$( "#users tbody" ).append( "<tr>" +
@@ -226,8 +237,8 @@
 							allFields.val( "" ).removeClass( "ui-state-error" );
 						}
 					});
-
-					$( "#dialog-form" ).dialog( "open" );
+*/
+					$dialog.dialog( 'open' );
 			}
 			
 		</script>
@@ -293,24 +304,9 @@
 					</div>
 				</div>
 			</div>
-		<div id="footer">
-			<img id="footerButtons" src="images/footerbuttons.png" />
+			<div id="footer">
+				<img id="footerButtons" src="images/footerbuttons.png" />
+			</div>
 		</div>
-
-		<div id="dialog-form" title="We don't have that category yet">
-			<p class="validateTips">All form fields are required.</p>
-
-			<form>
-				<fieldset>
-					<label for="name">Name</label>
-					<input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
-					<label for="email">Email</label>
-					<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
-					<label for="password">Password</label> <input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
-				</fieldset>
-			</form>
-		</div>
-		
-	</div>
 	</body>
 </html>
